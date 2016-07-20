c486bfaa69caf0991ceab62f0428e1b0702615e5
Michael Schinis <mschinis@gmail.com>
2016-07-20 14:15:33 +0100

    feat(config): Updates ember-cli-deploy config file

--- blueprints/streethub-deploy-config/files/config/deploy.js
+++ blueprints/streethub-deploy-config/files/config/deploy.js
@@ -8,6 +8,92 @@ module.exports = function(deployTarget) {
 
   if (deployTarget === 'development') {
     ENV.build.environment = 'development';
+  }
+  if (deployTarget === 'staging') {
+    ENV.build = {
+        environment: 'staging',
+        outputPath: 'dist'
+    };
+    ENV.s3 = {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        bucket: '<staging-bucket>',
+        region: '<region>',
+        prefix: '<project>'
+    };
+    ENV['s3-index'] = {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        bucket: '<staging-bucket>',
+        region: '<region>',
+        allowOverwrite: true,
+        prefix: '<project>'
+    };
+    ENV.slack = {
+      webhookURL: process.env.SLACK_DEPLOY_WEBHOOK,
+      channel: '#logs',
+      username: 'wooosh!',
+      didDeploy: function(context) {
+        return function(slack) {
+          return slack.notify({
+            text: 'New deploy: <staging-url>!'
+          });
+        };
+      }
+    };
+  }
+  if (deployTarget === 'production') {
+    ENV.build = {
+        environment: 'production',
+        outputPath: 'dist'
+    };
+    ENV.s3 = {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        bucket: '<production-bucket>',
+        region: '<region>',
+        prefix: '<project>'
+    };
+    ENV['s3-index'] = {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        bucket: '<production-bucket>',
+        region: '<region>',
+        allowOverwrite: true,
+        prefix: '<project>'
+    };
+    ENV.slack = {
+      webhookURL: process.env.SLACK_DEPLOY_WEBHOOK,
+      channel: '#logs',
+      username: 'wooosh!',
+      didDeploy: function(context) {
+        return function(slack) {
+          return slack.notify({
+            text: 'New deploy: <production-url>!'
+          });
+        };
+      }
+    };
+  }
+
+  // Note: if you need to build some configuration asynchronously, you can return
+  // a promise that resolves with the ENV object instead of returning the
+  // ENV object synchronously.
+  return ENV;
+};
+
+
+
+/* jshint node: true */
+
+module.exports = function(deployTarget) {
+  var ENV = {
+    build: {}
+    // include other plugin configuration that applies to all deploy targets here
+  };
+
+  if (deployTarget === 'development') {
+    ENV.build.environment = 'development';
     // configure other plugins for development deploy target here
   }
 
@@ -74,7 +160,7 @@ module.exports = function(deployTarget) {
       didDeploy: function(context) {
         return function(slack) {
           return slack.notify({
-            text: 'New deploy: <project>!'
+            text: 'New deploy: <production-url>!'
           });
         };
       }
